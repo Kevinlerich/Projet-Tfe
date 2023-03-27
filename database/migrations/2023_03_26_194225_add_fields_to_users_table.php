692721055b35aa9f1a78ddbbdd410a8033af5893
ariel nana <arieldeguilique@hotmail.com>
2023-03-28 00:26:40 +0100

    database migrations and model ok

--- database/migrations/2023_03_26_194225_add_fields_to_users_table.php
+++ database/migrations/2023_03_26_194225_add_fields_to_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('ville');
-            $table->string('telephone');
+            $table->string('ville')->nullable();
+            $table->string('telephone')->nullable();
         });
     }
 
