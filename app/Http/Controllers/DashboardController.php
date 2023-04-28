4a47463e441d1a31dad1c8b33e6979a0bd720d54
ariel nana <arieldeguilique@hotmail.com>
2023-04-28 02:05:05 +0100

    update agenda

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Announce;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,7 +11,7 @@ class DashboardController extends Controller
     public function dashboard()
     {
         $annonces = Announce::query()->get();
-        $services = Announce::query()->get();
+        $services = Service::query()->get();
         return view('dashboard', compact('annonces', 'services'));
     }
 }
