a66aeccce89fb93de348544faf60b97e75f94c1e
ariel nana <arieldeguilique@hotmail.com>
2023-05-31 20:32:57 +0100

    update detail_annonce and detail_service

--- resources/views/frontend/detail_annonce.blade.php
+++ resources/views/frontend/detail_annonce.blade.php
@@ -23,30 +23,7 @@
 						<li class="text-center list-inline-item"><a class="fa fa-linkedin" target="_blank" href="https://www.linkedin.com/share?url={{ route('detail_annonce', $annonce->slug) }}"></a></li>
 					</ul>
 				</article>
-				@auth
-                @if ($annonce->user_id != Auth::user()->id)
-                <div class="block comment">
-					@auth
-                    <h4>Laisser un message</h4>
-					<form action="{{ route('contact_annonce') }}" method="post">
-                        @csrf
-						<!-- Message -->
-						<div class="form-group mb-30">
-                            <input type="hidden" name="destinataire_id" value="{{ $annonce->user->id }}">
-                            <input type="hidden" name="objet" value="{{ $annonce->id }}">
-							<label for="message">Message</label>
-							<textarea class="form-control" name="message" id="message" rows="8" required></textarea>
-						</div>
-						<button type="submit" class="btn btn-transparent">Laisser mon message</button>
-					</form>
-                    @endauth
 
-				</div>
-                @endif
-                @endauth
-                @guest
-                        <h4>Connectez-vous pour contacter cet annonceur</h4>
-                    @endguest
 			</div>
 		</div>
 	</div>
