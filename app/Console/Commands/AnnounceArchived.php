c8b0ce66a6bf11db6344f2c5778339ae84069845
ariel <arielnana913@gmail.com>
2023-07-05 18:12:52 +0100

    archived solved

--- app/Console/Commands/AnnounceArchived.php
+++ app/Console/Commands/AnnounceArchived.php
@@ -26,9 +26,10 @@ class AnnounceArchived extends Command
      */
     public function handle(): void
     {
-        $date_to_archived = \Carbon\Carbon::today()->subDays(30); // 30 jours
+        $date_to_archived = \Carbon\Carbon::today()->subDays(30)->toDateTimeString(); // 30 jours
         Announce::query()
-            ->where('created_at', '>=', $date_to_archived)
+            ->orWhere('created_at', '>=', $date_to_archived)
+            ->orWhere('created_at', '<=', $date_to_archived)
             ->update([
                 'archived' => 1
         ]);
