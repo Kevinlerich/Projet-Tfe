348d2e39017f47231b851b1f3cca7289a1254f8c
ariel <arielnana913@gmail.com>
2023-09-18 18:21:53 +0100

    list favoris

--- app/Http/Controllers/Client/FavorisController.php
+++ app/Http/Controllers/Client/FavorisController.php
@@ -13,7 +13,7 @@ class FavorisController extends Controller
     public function index()
     {
         $favoris = ChFavorite::query()->where('user_id', Auth::user()->id)->get();
-        return view('backend.favoris.index', compact('favoris'));
+        return view('backend.client.favoris.index', compact('favoris'));
     }
 
     public function destroy($id)
