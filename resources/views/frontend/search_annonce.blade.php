12770ecbd27d6e105cfaa656e2f458e8d38b6bfc
ariel <arielnana913@gmail.com>
2023-09-26 10:08:36 +0100

    update annonce display and insert detail in mail

--- resources/views/frontend/search_annonce.blade.php
+++ resources/views/frontend/search_annonce.blade.php
@@ -4,42 +4,42 @@
 @endsection
 @section('content')
 
-@include('layouts.search_annonce')
+    @include('layouts.search_annonce')
+    <section class="section-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9 col-md-8">
+                    <div class="product-grid-list">
+                        <div class="row mt-30">
+                            @foreach($annonces as $annonce)
+                                <div class="col-lg-4 col-md-6">
+                                    <!-- product card -->
+                                    <div class="product-item bg-light">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title"><a href="{{ route('detail_annonce', $annonce->slug) }}">{{ $annonce->titre }}</a></h4>
+                                                <ul class="list-inline product-meta">
+                                                    <li class="list-inline-item">
+                                                        <a href="{{ route('category_annonce', $annonce->category->slug) }}"><i class="fa fa-folder-open-o"></i>{{ $annonce->category->nom }}</a>
+                                                    </li>
+                                                    <li class="list-inline-item">
+                                                        <a href="{{ route('category_annonce', $annonce->category->slug) }}"><i class="fa fa-calendar"></i>{{ \Carbon\Carbon::parse($annonce->date_announce)->format('d-m-Y') }}</a>
+                                                    </li>
+                                                    <li class="list-inline-item">
+                                                        <a href="{{ route('category_annonce', $annonce->category->slug) }}"><i class="fa fa-location-arrow"></i>{{ $annonce->ville->nom }}</a>
+                                                    </li>
+                                                </ul>
+                                                <p class="card-text">{!! Str::substr($annonce->description,0,10) !!}</p>
 
-<section class="section-sm">
-	<div class="container">
-		<div class="row">
-
-			<div class="col-lg-9 col-md-8">
-				<!-- ad listing list  -->
-                @foreach($annonces as $annonce)
-				<div class="mt-20 ad-listing-list">
-                    <div class="p-4 row p-lg-3 p-sm-5">
-                        <div class="col-lg-8">
-                            <div class="row">
-                                <div class="col-lg-6 col-md-10">
-                                    <div class="ad-listing-content">
-                                        <div>
-                                            <a href="{{ route('detail_annonce', $annonce->slug) }}" class="font-weight-bold">{{ $annonce->titre }}</a>
+                                            </div>
                                         </div>
-                                        <ul class="mt-2 mb-3 list-inline">
-                                            <li class="list-inline-item"><a href="{{ route('category_annonce', $annonce->category->slug) }}"> <i class="fa fa-folder-open-o"></i> {{ $annonce->category->nom }}</a></li>
-                                            <li class="list-inline-item"><a href="{{ route('category_annonce', $annonce->category->slug) }}"><i class="fa fa-calendar"></i>{{ \Carbon\Carbon::parse($annonce->date_announce)->format('d-m-Y') }}</a></li>
-                                            <li class="list-inline-item"><a href="{{ route('category_annonce', $annonce->category->slug) }}"><i class="fa fa-location-arrow"></i>{{ $annonce->ville->nom }}</a></li>
-                                        </ul>
-                                        <p class="pr-5">
-                                            {!! Str::substr($annonce->description,0,10) !!}
-                                        </p>
                                     </div>
                                 </div>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
-                @endforeach
-			</div>
-		</div>
-	</div>
-</section>
-
+            </div>
+        </div>
+    </section>
 @endsection
